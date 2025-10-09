17ac36e8020a3fdb99137b1418301b927123cb6b
Harsh Amin <aminharsh317@gmail.com>
2025-10-09 16:35:29 -0700

    dbFlow stack

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -3,7 +3,7 @@ import * as cdk from "aws-cdk-lib";
 import { VpcStack } from "../lib/vpc-stack";
 import { DatabaseStack } from "../lib/database-stack";
 //import { ApiGatewayStack } from "../lib/api-stack";
-//import { DBFlowStack } from "../lib/dbFlow-stack";
+import { DBFlowStack } from "../lib/dbFlow-stack";
 //import { AmplifyStack } from "../lib/amplify-stack";
 //import { CICDStack } from "../lib/cicd-stack";
 
@@ -18,10 +18,20 @@ const StackPrefix = app.node.tryGetContext("StackPrefix");
 const environment = app.node.tryGetContext("environmentName");
 const version = app.node.tryGetContext("versionNumber");
 const githubRepo = app.node.tryGetContext("githubRepo");
+
 const vpcStack = new VpcStack(app, `${StackPrefix}-VpcStack`, {
   env,
   stackPrefix: StackPrefix,
 });
+
 const dbStack = new DatabaseStack(app, `${StackPrefix}-Database`, vpcStack, {
   env,
 });
+
+const dbFlowStack = new DBFlowStack(
+  app,
+  `${StackPrefix}-DBFlow`,
+  vpcStack,
+  dbStack,
+  { env }
+);
---
+++ cdk/lib/dbFlow-stack.ts
@@ -0,0 +1,103 @@
+import { Stack, StackProps, triggers } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { Duration } from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as iam from "aws-cdk-lib/aws-iam";
+import { VpcStack } from "./vpc-stack";
+import { DatabaseStack } from "./database-stack";
+
+export class DBFlowStack extends Stack {
+  constructor(
+    scope: Construct,
+    id: string,
+    vpcStack: VpcStack,
+    db: DatabaseStack,
+    props?: StackProps
+  ) {
+    super(scope, id, props);
+
+    // Create IAM role for Lambda within the VPC
+    const lambdaRole = new iam.Role(this, `${id}-lambda-vpc-role`, {
+      assumedBy: new iam.ServicePrincipal("lambda.amazonaws.com"),
+      description: "Role for all Lambda functions inside VPC",
+    });
+
+    // Add necessary policies to the Lambda role
+    lambdaRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          // Secrets Manager
+          "secretsmanager:GetSecretValue",
+          "secretsmanager:PutSecretValue",
+        ],
+        resources: [
+          `arn:aws:secretsmanager:${this.region}:${this.account}:secret:*`,
+        ],
+      })
+    );
+
+    lambdaRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          // CloudWatch Logs
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ],
+        resources: ["arn:aws:logs:*:*:*"],
+      })
+    );
+
+    lambdaRole.addToPolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: [
+          "ec2:CreateNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+        ],
+        resources: ["*"],
+      })
+    );
+
+    // Add additional managed policies
+    lambdaRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMReadOnlyAccess")
+    );
+
+    lambdaRole.addManagedPolicy(
+      iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess")
+    );
+
+    // Create a Lambda layer for node-pg-migrate
+    const nodePgMigrateLayer = new lambda.LayerVersion(
+      this,
+      "nodePgMigrateLayer",
+      {
+        code: lambda.Code.fromAsset("./layers/node-pg-migrate.zip"),
+        compatibleRuntimes: [lambda.Runtime.NODEJS_20_X],
+        description: "Lambda layer with node-pg-migrate and pg",
+      }
+    );
+
+    new triggers.TriggerFunction(this, `${id}-triggerLambda`, {
+      description: `Database initializer and migration runner - ${new Date().toISOString()}`,
+      functionName: `${id}-initializerFunction`,
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "index.handler",
+      timeout: Duration.seconds(300),
+      memorySize: 512,
+      environment: {
+        DB_SECRET_NAME: db.secretPathAdminName,
+        DB_USER_SECRET_NAME: db.secretPathUser.secretName,
+        DB_PROXY: db.secretPathTableCreator.secretName,
+      },
+      vpc: db.dbInstance.vpc,
+      code: lambda.Code.fromAsset("lambda/db_setup"),
+      layers: [nodePgMigrateLayer],
+      role: lambdaRole,
+    });
+  }
+}
